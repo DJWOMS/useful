7173cc1ea1d4724e2cf3fa1e4248d34b7a4bf7cc
DJWOMS <socanime@gmail.com>
2020-11-06 21:15:56 +0300

    fix get_current_user permission

--- src/app/auth/permissions.py
+++ src/app/auth/permissions.py
@@ -15,7 +15,7 @@ from .schemas import TokenPayload
 reusable_oauth2 = OAuth2PasswordBearer(tokenUrl="/api/v1/login/access-token")
 
 
-def get_current_user(token: str = Security(reusable_oauth2)):
+async def get_current_user(token: str = Security(reusable_oauth2)):
     """ Check auth user
     """
     try:
@@ -25,21 +25,21 @@ def get_current_user(token: str = Security(reusable_oauth2)):
         raise HTTPException(
             status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
         )
-    user = service.user_s.get_obj(id=token_data.user_id)
+    user = await service.user_s.get_obj(id=token_data.user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
 
 def get_user(current_user: User = Security(get_current_user)):
-    """Проверка активный юзер или нет"""
+    """ Проверка активный юзер или нет """
     if not current_user.is_active:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
 
 def get_superuser(current_user: User = Security(get_current_user)):
-    """Проверка суперюзер или нет"""
+    """ Проверка суперюзер или нет """
     if not current_user.is_superuser:
         raise HTTPException(
             status_code=400, detail="The user doesn't have enough privileges"
